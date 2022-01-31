ed22f19caf7ea6842841afe18b3a7e142b3d989b
mustafa <mustafa@MUHAMMAD-MUSTAF>
2022-01-31 19:30:38 +0500

    End of Sec 2 (get apis)

---
+++ API/Controllers/ActivitiesController.cs
@@ -0,0 +1,31 @@
+using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace API.Controllers
+{
+    public class ActivitiesController : BaseApiController
+    {
+        private readonly DataContext context;
+        public ActivitiesController(DataContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Activity>>> GetActivites(){
+
+             return await context.Activities.ToListAsync();
+            
+        }
+
+        [HttpGet("{id}")] //activity/id
+
+        public async Task<ActionResult<Activity>> GetActivity(Guid id){
+
+
+            return await context.Activities.FindAsync(id);
+        }
+    }
+}
